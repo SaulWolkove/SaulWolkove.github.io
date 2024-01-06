a808c3a68f245442e06753f50e9e4a48426b341b
Saul Wolkove <saul.wolkove@gmail.com>
2024-01-06 12:29:01 -0500

    again

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import styles from "./index.css";
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
-import Landing from "./components/Landing";
-import Banner from "./components/Banner";
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Banner from './components/Banner';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
-  useEffect(() => {
-    // Redirect to "/about" when the component mounts
-    useNavigate('/about');
-  }, [useNavigate]);
   return (
-      <Router>
-          <Banner/>
-          <Routes>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/experience" element={<Experience/>}/>
-            <Route path="/projects" element={<Projects/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-
-          </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <Navigate to="/about" replace />
+            </>
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
