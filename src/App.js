006d3386a193fba4947e670ffa872a0805e313f3
Saul Wolkove <saul.wolkove@gmail.com>
2024-01-06 12:30:17 -0500

    redid

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Banner from './components/Banner';
-import About from './components/About';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import styles from "./index.css";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import Landing from "./components/Landing";
+import Banner from "./components/Banner";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Banner />
-              <Navigate to="/about" replace />
-            </>
-          }
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+      <Router>
+          <Banner/>
+          <Routes>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/experience" element={<Experience/>}/>
+            <Route path="/projects" element={<Projects/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+
+          </Routes>
+      </Router>
   );
 }
 
